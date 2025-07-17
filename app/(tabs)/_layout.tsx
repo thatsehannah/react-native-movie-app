159c9e0d3bd6fe76ae36010fce4dfc5f46bd754a
E. Hannah <echannah631@gmail.com>
2025-07-17 12:34:19 -0400

    creating reusable tabicon component, adding screenoption styles to tabs component in tablayout, defining the tabs with more options

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,49 +2,128 @@ import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Tabs } from "expo-router";
 import React from "react";
-import { Image, ImageBackground, Text } from "react-native";
+import { Image, ImageBackground, Text, View } from "react-native";
 
-const _layout = () => {
+const TabIcon = ({
+  focused,
+  icon,
+  name,
+}: {
+  focused: boolean;
+  icon: any;
+  name: string;
+}) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className='flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden'
+      >
+        <Image
+          source={icon}
+          tintColor='#151312'
+          className='size-5'
+        />
+        <Text className='text-secondary text-base font-semibold ml-2'>
+          {name}
+        </Text>
+      </ImageBackground>
+    );
+  }
+
   return (
-    <Tabs>
+    <View className='size-full justify-center items-center mt-4 rounded-full'>
+      <Image
+        source={icon}
+        tintColor='#a8b5db'
+        className='size-5'
+      />
+    </View>
+  );
+};
+
+const TabLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0d23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23",
+        },
+      }}
+    >
       <Tabs.Screen
         name='index'
         options={{
           title: "Home",
           headerShown: false,
           tabBarIcon: ({ focused }) => (
-            <>
-              <ImageBackground
-                source={images.highlight}
-                className='flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden'
-              >
-                <Image
-                  source={icons.home}
-                  tintColor='#151312'
-                  className='size-5'
-                />
-                <Text className='text-secondary text-base font-semibold ml-2'>
-                  Home
-                </Text>
-              </ImageBackground>
-            </>
+            <TabIcon
+              focused={focused}
+              icon={icons.home}
+              name='Home'
+            />
           ),
         }}
       />
       <Tabs.Screen
         name='search'
-        options={{ title: "Search", headerShown: false }}
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.search}
+              name='Search'
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='saved'
-        options={{ title: "Saved", headerShown: false }}
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.save}
+              name='Saved'
+            />
+          ),
+        }}
       />
       <Tabs.Screen
         name='profile'
-        options={{ title: "Profile", headerShown: false }}
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={icons.person}
+              name='Profile'
+            />
+          ),
+        }}
       />
     </Tabs>
   );
 };
 
-export default _layout;
+export default TabLayout;
