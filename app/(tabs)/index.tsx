d3f88122bbf71a1dfb548898f27b02532bc3ed7e
E. Hannah <echannah631@gmail.com>
2025-07-17 13:06:57 -0400

    starting to build out the homepage, stubbing on the search bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ImageBackground, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className='flex-1 justify-center items-center'>
-      <Text className='text-5xl font-bold text-light-200'>Welcome!</Text>
+    <View className='flex-1 bg-primary'>
+      <ImageBackground
+        source={images.bg}
+        className='flex-1'
+      >
+        <ScrollView
+          className='flex-1 px-5'
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+        >
+          <Image
+            source={icons.logo}
+            className='w-12 h-10 mt-20 mb-5 mx-auto'
+          />
+          <View className='flex-1 mt-5'>
+            <SearchBar />
+          </View>
+        </ScrollView>
+      </ImageBackground>
     </View>
   );
 }
