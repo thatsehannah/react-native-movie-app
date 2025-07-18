97fe1594cd36d3c2625efe19b1417b329820a6e7
E. Hannah <echannah631@gmail.com>
2025-07-18 16:14:33 -0400

    hiding the status bar, fixing screen name in layout file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import "./global.css";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name='(tabs)'
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name='movie/id'
-        options={{ headerShown: false }}
-      />
-    </Stack>
+    <>
+      <StatusBar hidden />
+      <Stack>
+        <Stack.Screen
+          name='(tabs)'
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name='movies/[id]'
+          options={{ headerShown: false }}
+        />
+      </Stack>
+    </>
   );
 }
