9d3b281f3028b1923af2f4ee5b1ffb86f48ed610
E. Hannah <echannah631@gmail.com>
2025-07-18 11:29:44 -0400

    implementing a list empty component to search screen for when no movies are returned

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -102,6 +102,17 @@ const Search = () => {
                 )}
             </>
           }
+          ListEmptyComponent={
+            !moviesLoading && !moviesError ? (
+              <View className='mt-10 px-5'>
+                <Text className='text-center text-gray-500'>
+                  {searchQuery.trim()
+                    ? "No movies found"
+                    : "Search for a movie"}
+                </Text>
+              </View>
+            ) : null
+          }
         />
       </ImageBackground>
     </View>
